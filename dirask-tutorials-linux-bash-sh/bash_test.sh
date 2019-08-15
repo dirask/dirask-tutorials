e337ba4888af2af83e07e2c071a731fbf2270a13
dirask <dirask@gmail.com>
2019-08-15 15:51:05 +0200

    dirask-tutorials-linux-bash-sh

---
+++ dirask-tutorials-linux-bash-sh/bash_test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# print to console
+echo 'Hello World'
+
+# will wait until user press Enter
+read
+
+# reference:
+# https://dirask.com/q/bash-create-simple-sh-script-with-header-MDgX5p
---
+++ dirask-tutorials-linux-bash-sh/pause_bash_sh_script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# print to console
+echo 'Hello World'
+
+echo 'Will wait until user press Enter'
+
+# will wait until user press Enter
+read
+
+# reference:
+# https://dirask.com/q/bash-shell-sh-script-with-do-not-close-terminal-which-works-like-windows-bat-pause-PpOVJD
