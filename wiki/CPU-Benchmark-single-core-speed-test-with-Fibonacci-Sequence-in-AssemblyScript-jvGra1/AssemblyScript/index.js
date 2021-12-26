c1fad65a78ffc4a448f30096626099e9802fc7e6
undqurek <undqurek@gmail.com>
2021-12-26 21:59:31 +0100

    AssemblyScript result reading fix

--- wiki/CPU-Benchmark-single-core-speed-test-with-Fibonacci-Sequence-in-AssemblyScript-jvGra1/AssemblyScript/index.js
+++ wiki/CPU-Benchmark-single-core-speed-test-with-Fibonacci-Sequence-in-AssemblyScript-jvGra1/AssemblyScript/index.js
@@ -6,6 +6,11 @@ const path = __dirname + '/build/optimized.wasm';
 const source = fs.readFileSync(path);
 const instance = loader.instantiateSync(source, imports);
 
-const result = instance.exports.test();
+const pointer = instance.exports.test();
+const array = instance.exports.__getInt64Array(pointer);
 
-console.log(`Time: ${0.001 * result}s`);
+const time = Number(array[0]);   // time in ms
+const result = Number(array[1]); // b variable
+
+console.log(`Time: ${0.001 * time}s`);
+console.log(`Result: ${result}`);
